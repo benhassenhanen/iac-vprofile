04dbd41cc20c5710da4d4bdb8bec73443774686d
benhassenhanen <benhassenhanen@gmail.com>
2025-03-13 10:25:30 +0100

    update

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,13 +27,11 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
+    bucket = "terraformstate381492080136"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "eu-central-1"
   }
 
   required_version = "~> 1.6.3"
 }
-##
-##
-##
+
